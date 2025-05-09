e01569677a8bd6e1e8abb5b7f0bdd73e4a3a9259
Brandon Sneed <brandon@redf.net>
2025-05-09 16:20:39 -0700

    Updated adjust to latest.

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         .package(
             name: "AdjustSdk",
             url: "https://github.com/adjust/ios_sdk.git",
-            from: "5.0.1")
+            from: "5.4.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
